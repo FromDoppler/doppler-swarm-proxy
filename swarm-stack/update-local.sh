6172bd671c8a68900f988276a89f5aa6efe0497b
Andrés Moschini <andresmoschini@gmail.com>
2019-11-15 13:47:05 -0300

    chore(build): add our common bash initialization script to update-local.sh

--- swarm-stack/update-local.sh
+++ swarm-stack/update-local.sh
@@ -1,3 +1,20 @@
+#!/bin/sh
+
+# Stop script on NZEC
+set -e
+# Stop script if unbound variable found (use ${var:-} if intentional)
+set -u
+
+# Lines added to get the script running in the script path shell context
+# reference: http://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+cd $(dirname $0)
+
+# To avoid issues with MINGW and Git Bash, see:
+# https://github.com/docker/toolbox/issues/673
+# https://gist.github.com/borekb/cb1536a3685ca6fc0ad9a028e6a959e3
+export MSYS_NO_PATHCONV=1
+export MSYS2_ARG_CONV_EXCL="*"
+
 docker stack rm doppler-swarm
 docker build -t fromdoppler/sites-proxy:local ../sites-proxy
 # In place of the previous line, I tried this:
